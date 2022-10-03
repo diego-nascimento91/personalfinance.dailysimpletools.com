8d8b85a3d22d9ff38ef557f4876720896b74ab84
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-03 20:59:15 -0300

    alterado estilização das transações para mobile

--- src/pages/Home/Transactions/TransactionSummary/TransactionSummary.tsx
+++ src/pages/Home/Transactions/TransactionSummary/TransactionSummary.tsx
@@ -20,11 +20,13 @@ const TransactionSummary = (props: Props) => {
   return (
     <div className={styles.transaction__container}>
       <div className={styles.transaction__payment}>{transaction.payment}</div>
-      <div className={styles.transaction__description}>
-        <p>{transaction.description}</p>
-        <p className={styles['transaction__description--price']}>R$ {(+transaction.price).toFixed(2)}</p>
+      <div className={styles.transaction__body}>
+        <div className={styles.transaction__description__date}>
+          <p className={styles.transaction__description}>{transaction.description}</p>
+          <p className={styles.transaction__date}>{formatDate(transaction.date)}</p>
+        </div>
+        <div className={styles['transaction__description--price']}>R$ {(+transaction.price).toFixed(2)}</div>
       </div>
-      <div className={styles.transaction__date}>{formatDate(transaction.date)}</div>
     </div>
   );
 };

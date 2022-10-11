b0deee4ebfea28eb7e1420c69f0d213f2b9a80f4
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-11 09:10:33 -0300

    code formatting

--- src/pages/Home/Welcome/Welcome.tsx
+++ src/pages/Home/Welcome/Welcome.tsx
@@ -2,6 +2,7 @@ import { useUser } from 'assets/state/hooks/useUser';
 
 const Welcome = () => {
   const [user,] = useUser();
+  
   return (
     <section className='theme__homesections'>
       <p>Welcome {user?.email}!</p>

1e91dae207ff80dc4f3a1bdf4a77fa8ecc1e6700
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-13 20:30:30 -0300

    adicionado botão seeall no componente Recent Transactions

--- src/pages/Home/RecentTransactions/RecentTransactions.tsx
+++ src/pages/Home/RecentTransactions/RecentTransactions.tsx
@@ -1,4 +1,5 @@
 import { ITransaction } from 'assets/interfaces/interfaces';
+import { Link } from 'react-router-dom';
 import styles from './RecentTransactions.module.scss';
 import TransactionSummary from './TransactionSummary/TransactionSummary';
 
@@ -6,6 +7,7 @@ const RecentTransactions = ({ transactions }:{ transactions?: ITransaction[] }) 
 
   return (
     <section className={`${styles.transactions__container} theme__homesections`}>
+      <Link to="/transactions" className={styles.transactions__seeall}>see all</Link>
       <h2 className={styles.transactions__title}>Recent Transactions</h2>
       {
         transactions && transactions.length > 0 && transactions[0].id !== ''

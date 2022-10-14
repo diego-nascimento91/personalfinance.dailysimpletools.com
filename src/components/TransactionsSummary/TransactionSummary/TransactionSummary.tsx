4b2c32cf6d974fdfebb1e3b2c8524f7af63b17d3
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-14 00:12:31 -0300

    TransactionSummary: refeito estilização

--- src/components/TransactionsSummary/TransactionSummary/TransactionSummary.tsx
+++ src/components/TransactionsSummary/TransactionSummary/TransactionSummary.tsx
@@ -3,33 +3,33 @@ import classNames from 'classnames';
 import styles from './TransactionSummary.module.scss';
 
 
-const TransactionSummary = ({ transaction }:{ transaction: ITransaction }) => {
+const TransactionSummary = ({ transaction }: { transaction: ITransaction }) => {
 
   const formatDate = (date: Date) => {
     const day = date.getUTCDate();
-    const month = date.getUTCMonth() + 1;
-    const year = date.getFullYear();
-    const dateString = `${day}/${month}/${year}`;
+    const month = date.toLocaleString('default', { month: 'short' });
+    const dateString = `${day < 10 ? '0' + day : day}/${month.replace(/\./g, '')}`;
 
     return dateString;
   };
 
   return (
     <div className={styles.transaction__container}>
-      <div className={styles.transaction__payment}>{transaction.payment}</div>
-      <div className={styles.transaction__body}>
-        <div className={styles.transaction__description__date}>
-          <p className={styles.transaction__description}>{transaction.description}</p>
-          <p className={styles.transaction__date}>{formatDate(transaction.date)}</p>
-        </div>
-        <div className={classNames({
-          [styles.transaction__price]: true,
-          [styles.transaction__price__positive]: transaction.type === 'income',
-          [styles.transaction__price__negative]: transaction.type === 'expense',
-        })}>
-          { transaction.type === 'expense' ? '- ' : '+ '}
-          R$ {(+transaction.price).toFixed(2)}</div>
+      <div className={styles.transaction__category}>
+        <p className={styles['transaction__category--icon']}>{transaction.category[0]}</p>
       </div>
+      <div className={styles.transaction__body}>
+        <p className={styles.transaction__place}>{transaction.place}</p>
+        <p className={
+          classNames({
+            [styles.transaction__price]: true,
+            [styles.transaction__price__positive]: transaction.type === 'income',
+            [styles.transaction__price__negative]: transaction.type === 'expense'
+          })}>
+          {transaction.type === 'expense' ? '- ' : '+ '} R$ {(+transaction.price).toFixed(2)}
+        </p>
+      </div>
+      <p className={styles.transaction__date}>{formatDate(transaction.date)}</p>
     </div>
   );
 };

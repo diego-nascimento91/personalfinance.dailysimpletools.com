2c98cbab23c594feb6d253fefa23d1e77c16eb68
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-15 17:07:06 -0300

    TransactionsPage: adicionado componente DatePicker. Alterado recoil-state useTransactionsAll para useTransactionsMonth. Alterado chamada da funcao handleFetchTransactionsAll para handleFetchTransactionsMonth.

--- src/pages/TransactionsPage/TransactionsPage.tsx
+++ src/pages/TransactionsPage/TransactionsPage.tsx
@@ -1,33 +1,37 @@
 import ExpensePerCategory from 'components/ExpensePerCategory/ExpensePerCategory';
-import { handleFetchCategories, handleFetchTransactionsAll } from 'assets/functions/fetchFunctions';
+import { handleFetchCategories, handleFetchTransactionsMonth } from 'assets/functions/fetchFunctions';
 import { useCategories } from 'assets/state/hooks/useCategories';
-import { useTransactionsAll } from 'assets/state/hooks/useTransactionsAll';
 import { useUser } from 'assets/state/hooks/useUser';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TransactionsSummary from 'components/TransactionsSummary/TransactionsSummary';
 import NavBar from 'components/NavBar/NavBar';
+import { useTransactionsMonth } from 'assets/state/hooks/useTransactionsMonth';
+import { useChosenMonth } from 'assets/state/hooks/useChosenMonth';
+import DatePicker from 'components/DatePicker/DatePicker';
 
 const TransactionsPage = () => {
   const nav = useNavigate();
   const [user, loading] = useUser();
-  const [transactionsAll, setTransactionsAll] = useTransactionsAll();
+  const [transactionsMonth, setTransactionsMonth] = useTransactionsMonth();
   const [, setCategories] = useCategories();
+  const [month] = useChosenMonth();
 
   useEffect(() => {
     if (loading) return;
     if (!user) nav('/');
     if (user) {
-      handleFetchTransactionsAll(`users/${user?.uid}/transactions`, setTransactionsAll);
       handleFetchCategories('basicCategories', setCategories);
+      handleFetchTransactionsMonth(`users/${user?.uid}/transactions`, month, setTransactionsMonth);
     }
-  }, [user, loading]);
+  }, [user, loading, month]);
 
   return(
     <div className='theme__padding'>
       <NavBar />
-      <ExpensePerCategory transactions={transactionsAll}/>
-      <TransactionsSummary transactions={transactionsAll} allTransactions={true}/>
+      <DatePicker />
+      <ExpensePerCategory transactions={transactionsMonth}/>
+      <TransactionsSummary transactions={transactionsMonth} allTransactions={true}/>
     </div>
   );
 };

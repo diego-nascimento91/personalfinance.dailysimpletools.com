770a79ef57489f37a66c42da7abc927d85602606
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-15 17:55:30 -0300

    fetchFunctions: retirado objetos props e interce Props como parametro da funcao fetchFunction e deixado as variaveis diretamente como parametros

--- src/assets/functions/fetchFunctions.ts
+++ src/assets/functions/fetchFunctions.ts
@@ -2,30 +2,30 @@ import FirebaseFirestoreService from 'assets/functions/FirebaseFirestoreService'
 import { ITransaction, IQuery, ICategory } from 'assets/interfaces/interfaces';
 import { SetterOrUpdater } from 'recoil';
 
-// fetchTransactions function
-interface Props {
-  collectionPath: string, 
-  setTransactions: SetterOrUpdater<ITransaction[]> | React.Dispatch<React.SetStateAction<ITransaction[]>>,
-  queries?: IQuery[],
-}
-export const fetchFunction = async (props: Props) => {
-  const { collectionPath, setTransactions, queries } = props;
+// fetch function
+export const fetchFunction =
+  async (
+    collectionPath: string,
+    setTransactions: SetterOrUpdater<ITransaction[]> | React.Dispatch<React.SetStateAction<ITransaction[]>>,
+    queries?: IQuery[],
+  ) => {
+    // const { collectionPath, setTransactions, queries } = props;
 
-  try {
-    const response = await FirebaseFirestoreService.readAllDocsFromCollection(collectionPath, 'date', 'desc', queries);
-    setTransactions(response as ITransaction[]);
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error);
-      alert(error.message);
+    try {
+      const response = await FirebaseFirestoreService.readAllDocsFromCollection(collectionPath, 'date', 'desc', queries);
+      setTransactions(response as ITransaction[]);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error);
+        alert(error.message);
+      }
     }
-  }
-};
+  };
 
 
 // call fetchTransactions with queries to get all transactions
-export const handleFetchTransactionsAll = (collectionPath: string,  setTransactions: SetterOrUpdater<ITransaction[]> | React.Dispatch<React.SetStateAction<ITransaction[]>>) => {
-  fetchFunction( { collectionPath, setTransactions } );
+export const handleFetchTransactionsAll = (collectionPath: string, setTransactions: SetterOrUpdater<ITransaction[]> | React.Dispatch<React.SetStateAction<ITransaction[]>>) => {
+  fetchFunction(collectionPath, setTransactions);
 };
 
 
@@ -51,7 +51,7 @@ export const handleFetchTransactionsMonth = (collectionPath: string, month: Date
   const { firstDay, lastDay } = formatDate(month);
   const queries = getQueries(firstDay, lastDay);
 
-  fetchFunction({ collectionPath, setTransactions: setTransactionsMonth, queries });
+  fetchFunction(collectionPath, setTransactionsMonth, queries);
 };
 
 const getQueries = (firstDay: Date, lastDay: Date) => {

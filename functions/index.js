ac8cdbdec8653d266def55f8b855ff1dba9869d2
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-17 14:53:34 -0300

    functions: corrigido bug getAll(array) para getAll(...array)

--- functions/index.js
+++ functions/index.js
@@ -13,39 +13,30 @@ exports.onDeleteUser = functions.auth.user().onDelete(async (user) => {
     const userRef = firestore.collection('users').doc(user.uid);
 
     // get user's subcollections
-    let subCollections;
-    try {
-      subCollections = await userRef.listCollections();
-    } catch (error) {
-      console.log(error.message);
-      throw error;
-    }
+    const subCollections = await userRef.listCollections();
 
     // for each subcollection, get the docs e delete each one of them
     for (const subCollection of subCollections) {
       console.log(`Found subcollection with id: ${subCollection.id}`);
-
       const subCollectionRef = firestore.collection(`users/${user.uid}/${subCollection.id}`);
-      try {
-        const listDocsRefs = await subCollectionRef.listDocuments();
-        const listDocs = await firestore.getAll(listDocsRefs);
-
-        for (let doc of listDocs) {
-          if (doc.exists) {
-            console.log(`Found document with data: ${doc.id}`);
-            await subCollectionRef.doc(doc.id).delete();
-          }
+      const listDocsRefs = await subCollectionRef.listDocuments();
+      const listDocs = await firestore.getAll(...listDocsRefs);
+      for (let doc of listDocs) {
+        if (doc.exists) {
+          console.log(`Found document with data: ${doc.id}`);
+          await subCollectionRef.doc(doc.id).delete();
         }
-
-      } catch (error) {
-        console.log(error.message);
-        throw error;
       }
     }
-
     // delete user document
     userRef.delete();
   };
 
-  deleteUserData();
+  try {
+    await deleteUserData();
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+  console.log('Subcollections and documents deleted successfully!')
 });

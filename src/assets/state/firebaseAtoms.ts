202d615bbe555563e73fc42a04244fea879a1630
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-18 10:34:04 -0300

    removido useUser atom, pois nao era utilizado em lugar nenhum

--- src/assets/state/firebaseAtoms.ts
+++ src/assets/state/firebaseAtoms.ts
@@ -1,11 +1,5 @@
 import { atom } from 'recoil';
-import firebase from 'firebase/compat/app';
 import { ICategory, ITransaction } from 'assets/interfaces/interfaces';
-
-export const userAtom = atom<firebase.User | null> ({
-  key: 'userAtom',
-  default: null
-});
 
 export const useTransactionsMonthAtom = atom<ITransaction[]> ({
   key: 'useTransactionsMonthAtom',

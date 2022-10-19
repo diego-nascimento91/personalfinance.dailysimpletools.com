e959a0367f03c9f4a39be6182e2007434e21e756
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-19 10:57:59 -0300

    adicionado opcao de adicionar transacao pela TransactionsPage. TransactionsPage: adicionado componente AddTransactionButton. E adicionado estilização similar ao Home.

--- src/pages/TransactionsPage/TransactionsPage.tsx
+++ src/pages/TransactionsPage/TransactionsPage.tsx
@@ -6,6 +6,8 @@ import ExpensePerCategory from 'components/ExpensePerCategory/ExpensePerCategory
 import TransactionsSummary from 'components/TransactionsSummary/TransactionsSummary';
 import NavBar from 'components/NavBar/NavBar';
 import DatePicker from 'components/DatePicker/DatePicker';
+import styles from './TransactionsPage.module.scss';
+import AddTransactionButton from 'components/AddTransactionButton/AddTransactionButton';
 
 const TransactionsPage = () => {
   const nav = useNavigate();
@@ -23,22 +25,25 @@ const TransactionsPage = () => {
   }, [user, loading]);
 
   const handleUpdateDBs = () => {
-    if(user){
-      if(!(categories && categories.length > 0)){
+    if (user) {
+      if (!(categories && categories.length > 0)) {
         handleFetchCategories(user.uid, setCategories);
       }
-      if(!(transactionsMonth && transactionsMonth.length > 0)){
+      if (!(transactionsMonth && transactionsMonth.length > 0)) {
         handleFetchTransactionsMonth(user.uid, setTransactionsMonth, month);
       }
     }
   };
 
-  return(
-    <div className='theme__padding'>
-      <NavBar />
-      <DatePicker />
-      <ExpensePerCategory transactions={transactionsMonth}/>
-      <TransactionsSummary transactions={transactionsMonth} allTransactions={true}/>
+  return (
+    <div className={`theme__padding ${styles.transactionspage}`}>
+      <div>
+        <NavBar />
+        <DatePicker />
+        <ExpensePerCategory transactions={transactionsMonth} />
+        <TransactionsSummary transactions={transactionsMonth} allTransactions={true} />
+      </div>
+      <AddTransactionButton />
     </div>
   );
 };

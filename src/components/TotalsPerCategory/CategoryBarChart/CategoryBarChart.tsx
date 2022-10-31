76507f6da39e3c9da5757b653c6a9317097a24ef
Diego Ferreira <diego.nascimento91@gmail.com>
2022-10-31 17:39:50 -0300

    CategoryBarChart: adicionado tratamento de link incorreto para imagem.

--- src/components/TotalsPerCategory/CategoryBarChart/CategoryBarChart.tsx
+++ src/components/TotalsPerCategory/CategoryBarChart/CategoryBarChart.tsx
@@ -1,6 +1,7 @@
 import { ITotalsCategories } from 'assets/interfaces/interfaces';
 import { useFilteredCategory } from 'assets/state/hooks/filterTransactionsHooks';
 import classNames from 'classnames';
+import { useState } from 'react';
 import styles from './CategoryBarChart.module.scss';
 
 interface Props {
@@ -10,7 +11,8 @@ interface Props {
 }
 const CategoryBarChart = ({ barHeight, totalCategory, allTransactions }: Props) => {
   const [filteredCategory, setFilteredCategory] = useFilteredCategory();
-  
+  const [imgError, setImgError] = useState(false);
+
   const getHeightPercentage = () => {
     const heightPercentage = (barHeight * 100).toString() + '%';
     return heightPercentage;
@@ -48,7 +50,7 @@ const CategoryBarChart = ({ barHeight, totalCategory, allTransactions }: Props) 
             [styles['barChart__bar--containerRelativeSizeCursor']]: allTransactions,
             [styles['barChart__bar--containerRelativeSizeNotSelected']]: allTransactions && filteredCategory && totalCategory.id !== filteredCategory.id,
           })}
-          onClick={() => {if(allTransactions) handleSetFilteredCategory();}}
+          onClick={() => { if (allTransactions) handleSetFilteredCategory(); }}
         >
           <p className={styles['barChart__bar--amountMoney']}>
             <span className={classNames({
@@ -56,15 +58,24 @@ const CategoryBarChart = ({ barHeight, totalCategory, allTransactions }: Props) 
             })}>${getNumberVisual()}</span>
           </p>
           {
-            totalCategory.icon && totalCategory.icon.length > 0 
+            totalCategory.icon && totalCategory.icon.length > 0
               ? (
-                <img className={styles['barChart__bar--icon']} src={totalCategory.icon} />
+                <>
+                  <img className={styles['barChart__bar--icon']} src={totalCategory.icon}
+                    onError={({ currentTarget }) => {
+                      currentTarget.src = '';
+                      currentTarget.className = `${styles['barChart__bar--icon']} ${styles['imgError']}`;
+                      setImgError(true);
+                    }}
+                  />
+                  {imgError && <span className={styles['barChart__bar--iconText']}>{totalCategory.name[0]}</span>}
+                </>
               )
               : (
                 <span className={styles['barChart__bar--iconText']}>{totalCategory.name[0]}</span>
               )
           }
-          
+
         </div>
       </div>
       <div className={styles['barChart__categoryName--container']}>

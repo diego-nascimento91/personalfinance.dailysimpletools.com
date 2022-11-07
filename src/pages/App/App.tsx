33976e3718bdcdead95914974e4203292a08b659
Diego Ferreira <diego.nascimento91@gmail.com>
2022-11-07 11:06:42 -0300

    AuthPages: criado pasta e colocado todas as paginas referentes a login/registro nessa pasta

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import MenuST from 'components/MenuST/MenuST';
 import Footer from 'components/Footer/Footer';
-import Login from 'pages/Login/Login';
-import Register from 'pages/Register/Register';
-import ResetPassword from 'pages/ResetPassword/ResetPassword';
-import PasswordReseted from 'pages/ResetPassword/PasswordReseted/PasswordReseted';
+import ResetPassword from 'pages/AuthPages/ResetPassword/ResetPassword';
+import PasswordReseted from 'pages/AuthPages/ResetPassword/PasswordReseted/PasswordReseted';
 import Home from 'pages/Home/Home';
 import TransactionsPage from 'pages/TransactionsPage/TransactionsPage';
 import AddTransactionForm from 'pages/AddTransaction/AddTransaction';
 import AddCategory from 'pages/AddCategory/AddCategory';
+import Login from 'pages/AuthPages/Login/Login';
+import Register from 'pages/AuthPages/Register/Register';
 
 function App() {
   return (

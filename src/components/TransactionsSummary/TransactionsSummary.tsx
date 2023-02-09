fd14ce5db4233ed951f9abf87be486ee0bfc8bbf
Diego Ferreira <diego.nascimento91@gmail.com>
2023-02-09 14:49:19 -0300

    refact: TransactionsSummary: criado função filterTransactions para filtrar o array transactions, recebido como props, de acordo com o props allTransactions, e com o state filteredCategory. Essa funcionalidade estava dentro do jsx no return.

--- src/components/TransactionsSummary/TransactionsSummary.tsx
+++ src/components/TransactionsSummary/TransactionsSummary.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ITransaction } from 'assets/interfaces/interfaces';
 import { useFilteredCategory } from 'assets/state/hooks/filterTransactionsHooks';
+import { useEffect, useState } from 'react';
 import styles from './TransactionsSummary.module.scss';
 import stylesComponents from 'assets/styles/pageComponents.module.scss';
 import TransactionSummary from './TransactionSummary/TransactionSummary';
@@ -10,29 +11,40 @@ interface Props {
   allTransactions?: boolean
 }
 const TransactionsSummary = (props: Props) => {
-  const { transactions, allTransactions = false } = props;
-
+  const {transactions, allTransactions = false} = props;
   const [filteredCategory] = useFilteredCategory();
+  const [filteredTransactions, setFilteredTransactions] = useState<ITransaction[]>();
+
+  useEffect(() => {
+    filterTransactions();
+  }, [filteredCategory]);
+
+  const filterTransactions = () => {
+    let thisFilteredTransactions: ITransaction[] = [];
+
+    if(!allTransactions) {
+      thisFilteredTransactions = transactions.slice(0, 4);
+    } else {
+      if (filteredCategory) {
+        thisFilteredTransactions = transactions.filter(item => (item.category === filteredCategory.name));
+      } else {
+        thisFilteredTransactions = transactions;
+      }
+    }
+    
+    setFilteredTransactions(thisFilteredTransactions);
+  };
 
   return (
     <section className={`${styles.transactions__container} ${stylesComponents.pageComponents}`}>
       {!allTransactions && <Link to="/transactions" className={styles.transactions__seeall}>see all</Link>}
       <h2 className={`theme__title ${styles.transactions__title}`}>{allTransactions ? 'Transactions' : 'Recent Transactions'}</h2>
       {
-        transactions && transactions.length > 0
+        filteredTransactions && filteredTransactions.length > 0 
           ? (
-            transactions.map((transaction, index) => {
-              if (!allTransactions) {
-                if (index < 4)
-                  return (<TransactionSummary transaction={transaction} key={transaction.id} />);
-              }
-              else {
-                if (!filteredCategory)
-                  return (<TransactionSummary transaction={transaction} key={transaction.id} />);
-                else if (filteredCategory && filteredCategory.name === transaction.category)
-                  return (<TransactionSummary transaction={transaction} key={transaction.id} />);
-              }
-            })
+            filteredTransactions.map(item => (
+              <TransactionSummary transaction={item} key={item.id} />
+            ))
           )
           : <p>No transactions added yet</p>
       }
